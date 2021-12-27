7a594a261da8d555dc2731ffb739eba283a4da99
Erick Guzman <guzmi.dev@gmail.com>
2021-12-27 14:11:30 -0700

    footer finished

--- index.php
+++ index.php
@@ -71,5 +71,9 @@
     </div>
   </div>
   <!-- PIE DE PAGINA -->
+  <footer id="footer">
+    <p>Desarrollado por Erick Guzman &copy; <?= date('Y')?></p>
+    <p>Codigo del curso de Victor Robles WEB</p>
+  </footer>
 </body>
 </html>
