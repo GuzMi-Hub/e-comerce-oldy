970acd503e71b7e8e9ddee72f92d6213e5ab5d54
Erick Guzman <guzmi.dev@gmail.com>
2021-12-27 13:27:25 -0700

    Header finished

--- index.php
+++ index.php
@@ -4,17 +4,25 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./assets/css/styles.css">
   <title>Tienda de camisetas</title>
 </head>
 <body>
   <!-- CABECERA -->
-
+  <header id="header">
+    <div id="logo">
+      <img src="assets/img/camiseta.png" alt="Camiseta Logo">
+      <a href="index.php">
+        Tienda de camisetas
+      </a>
+    </div>
+  </header>
   <!-- MENU -->
 
   <!-- BARRA LATERAL -->
 
   <!-- CONTENIDO CENTRAL -->
-  
+
   <!-- PIE DE PAGINA -->
 </body>
 </html>
