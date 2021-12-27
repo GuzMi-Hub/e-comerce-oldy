1112f6b59eb875dbd75011db2d1cbe4cd74e4a42
Erick Guzman <guzmi.dev@gmail.com>
2021-12-27 13:31:55 -0700

    Menu finished

--- index.php
+++ index.php
@@ -18,7 +18,15 @@
     </div>
   </header>
   <!-- MENU -->
-
+  <nav id="menu">
+    <ul>
+      <li><a href="">Categoria 1</a></li>
+      <li><a href="">Categoria 2</a></li>
+      <li><a href="">Categoria 3</a></li>
+      <li><a href="">Categoria 4</a></li>
+      <li><a href="">Categoria 5</a></li>
+    </ul>
+  </nav>
   <!-- BARRA LATERAL -->
 
   <!-- CONTENIDO CENTRAL -->
